ebdaf6489b0b42e3f5f96d6826540f9701749709
Vlad Romila <romilavlad2001@gmail.com>
2020-03-23 00:38:15 +0200

    Done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 80
 const path = require("path");
 
+app.use(express.static(__dirname + '/build'));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
