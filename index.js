d31d8d1d109bea2cb70a27fa07ed2c4751f98f89
Vlad Romila <romilavlad2001@gmail.com>
2020-03-25 13:37:00 +0200

    Done

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const path = require("path");
 app.use(express.static(__dirname + '/build'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
+app.get('/silvia', (req, res) => res.sendFile(path.join(__dirname + '/build/silvia.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
