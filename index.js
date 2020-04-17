129c89b56069a072abefb71fd631616a11bef854
Romila Vlad <romilalvlad2001@gmail.com>
2020-04-18 00:12:31 +0300

    Done

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require("path");
 
 app.use(express.static(__dirname + '/build'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
+app.get('/', (req, res) => res.redirect("https://vladromila.ro/retrup"))
+app.get('/retrup', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
 app.get('/downloadApk', function (req, res) {
     const file = `${__dirname}/apk.apk`;
     res.download(file); // Set disposition and send it.
