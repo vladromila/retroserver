2bbff7995aa5ace5c65b667d004a5d2130daf19a
Romila Vlad <romilalvlad2001@gmail.com>
2020-04-17 22:57:58 +0300

    Done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require("path");
 app.use(express.static(__dirname + '/build'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
-app.get('/silvia', (req, res) => res.sendFile(path.join(__dirname + '/build/silvia.html')))
+app.get('/downloadApk', function (req, res) {
+    const file = `${__dirname}/apk.apk`;
+    res.download(file); // Set disposition and send it.
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
