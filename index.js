8deb0cc31c883fdedbbc911f30c4ad38313ecfd3
Romila Vlad <romilalvlad2001@gmail.com>
2020-04-20 19:10:17 +0300

    Done

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ app.get('/', (req, res) => {
 })
 app.use(express.static(__dirname + '/build'));
 
-app.get('/1', (req, res) => res.redirect('/retrup'))
 app.get('/retrup', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
-app.get('/downloadApk', function (req, res) {
-    const file = `${__dirname}/apk.apk`;
-    res.download(file); // Set disposition and send it.
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
