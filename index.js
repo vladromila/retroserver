a7e787038aafee55be8eddda09f9095b901d16c9
Vlad Romila <romilavlad2001@gmail.com>
2020-05-28 23:07:37 +0300

    Done

--- index.js
+++ index.js
@@ -12,4 +12,8 @@ app.use(express.static(__dirname + '/silvia'));
 app.get('/retrup', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
 app.get('/retrup/googleplay', (req, res) => res.redirect("https://play.google.com/store/apps/details?id=smarty.retrup.com"))
 app.get('/silvia', (req, res) => res.sendFile(path.join(__dirname + '/silvia/index.html')))
+app.get('/silviadownload', function(req, res){
+    const file = `${__dirname}/silvia/silvia.rar`;
+    res.download(file); // Set disposition and send it.
+  });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
